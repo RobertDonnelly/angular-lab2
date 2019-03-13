ea85c04fe6273ed6d6df91773bb3f0edfd591eab
ROBERT DONNELLY - STUDENT <G00358931@gmit.ie>
2019-03-13 17:36:56 +0000

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {MovieProvider} from '../../providers/movie/movie';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+movies:any=[];
+  constructor(public navCtrl: NavController,
+  private movieProvider:MovieProvider){
   }
 
+ionViewDidLoad(){
+  this.movieProvider.getFilmInfo().subscribe((data)=>
+  {
+    this.movies=data.Search;
+  });
 }
+
+}
+
+
